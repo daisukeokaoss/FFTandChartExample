de06f6d8a0b548f2fc1a816cc831b178953bba19
Daisuke Oka <daisukeokahassou@gmail.com>
2018-07-17 08:41:20 +0900

    first commit

---
+++ FFTExample2/GenerateSin.swift
@@ -0,0 +1,18 @@
+//
+//  GenerateSin.swift
+//  FFTExample2
+//
+//  Created by 岡大輔 on 2018/07/17.
+//  Copyright © 2018年 岡大輔. All rights reserved.
+//
+
+import UIKit
+
+class GenerateSin: NSObject {
+    
+    class func generateSinArray(count:Int)->Array<Double>
+    {
+        
+    }
+
+}
